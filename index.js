4b9f8cfc99e8458fb9076869e87805c2fca518ba
ivramana <venkataramana@inventcorp.com>
2024-04-06 13:31:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const port = process.env.port || 3000
+
+const app = express()
+app.get('/',(req,res)=> {
+    res.send("<h1>Welcome to node js sample application</h1>")
+})
+app.listen(port,()=>{
+    console.log("Application is running on port: ",port)
+})
